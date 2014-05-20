581d6da3f4a42532e4a2eea3f27527be0e431cfb
Chris Hansen <chansen@wanderful.com>
2014-05-19 17:34:48 -0700

    Add textfields to allow changing of filter and accuracy

--- speed/FSViewController.m
+++ speed/FSViewController.m
@@ -10,15 +10,18 @@
 
 #import <CoreLocation/CoreLocation.h>
 
-@interface FSViewController () <CLLocationManagerDelegate>
+@interface FSViewController () <CLLocationManagerDelegate, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *speedLabel;
 @property (weak, nonatomic) IBOutlet UIButton *locationButton;
+@property (weak, nonatomic) IBOutlet UITextField *accuracyTextField;
+@property (weak, nonatomic) IBOutlet UITextField *distanceFilterTextField;
 
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (strong, nonatomic) CLLocation *currentLocation;
 
 @property (nonatomic) BOOL updating;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomLayoutConstraint;
 
 @end
 
@@ -33,7 +36,9 @@
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
     self.locationManager.distanceFilter = 600.0f;
-//    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    
+    self.accuracyTextField.text = @"100";
+    self.distanceFilterTextField.text = @"600";
 }
 
 - (IBAction)toggleLocationServices:(UIButton *)sender {
@@ -50,6 +55,33 @@
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
     self.currentLocation = [locations lastObject];
     self.speedLabel.text = [NSString stringWithFormat:@"%g", self.currentLocation.speed];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.accuracyTextField resignFirstResponder];
+    [self.distanceFilterTextField resignFirstResponder];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [UIView animateWithDuration:.2 animations:^{
+        self.bottomLayoutConstraint.constant += 100;
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField == self.accuracyTextField) {
+        self.locationManager.desiredAccuracy = [textField.text doubleValue];
+    } else {
+        self.locationManager.distanceFilter = [textField.text doubleValue];
+    }
+    [self.locationManager stopUpdatingLocation];
+    [self.locationManager startUpdatingLocation];
+    [self.locationButton setTitle:@"Stop Updating" forState:UIControlStateNormal];
+    [UIView animateWithDuration:.2 animations:^{
+        self.bottomLayoutConstraint.constant -= 100;
+        [self.view layoutIfNeeded];
+    }];
 }
 
 @end
